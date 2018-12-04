63456d530685d9af3f44dc319c5ba8d1b9a69964
LeAlbus <jpedro_lopes21@homail.com>
2018-12-03 23:08:38 -0200

    Aula2 - Data pass Data persist

--- app/src/main/java/br/com/heiderlopes/calculaflex/ResultActivity.kt
+++ app/src/main/java/br/com/heiderlopes/calculaflex/ResultActivity.kt
@@ -2,11 +2,41 @@ package br.com.heiderlopes.calculaflex
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import br.com.heiderlopes.calculaflex.extension.format
+import kotlinx.android.synthetic.main.activity_form.view.*
+import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        calculate()
     }
+
+    private fun calculate() {
+        val gasPrice = intent.extras.getDouble("GAS_PRICE", 1.0)
+        val gasAverage = intent.extras.getDouble("GAS_AVERAGE", 1.0)
+        val ethanolPrice = intent.extras.getDouble("ETHANOL_PRICE", 1.0)
+        val ethanolAverage = intent.extras.getDouble("ETHANOL_AVERAGE", 1.0)
+
+        val performanceMyCar = ethanolAverage/gasAverage
+        val priceIndice = ethanolPrice/gasPrice
+
+        if(priceIndice <= performanceMyCar){
+            tvSuggestion.text = getString(R.string.label_ethanol)
+        } else {
+            tvSuggestion.text = getString(R.string.label_gasoline)
+        }
+
+        tvGasAverageResult.text = (gasPrice/gasAverage).format(2)
+        tvEthanolAverageResult.text = (ethanolPrice/ethanolAverage).format(2)
+
+        tvFuelRatio.text = getString(R.string.label_ratio_fuel, performanceMyCar.format(2))
+    }
+
 }
